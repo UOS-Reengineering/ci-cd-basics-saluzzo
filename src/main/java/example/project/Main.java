0e6bfdc0443081103bd45b1c1ff9b26ab0030184
jinyun <jli360@Sheffield.ac.uk>
2023-03-15 12:45:44 +0000

    Update Main.java
    wewe

--- src/main/java/example/project/Main.java
+++ src/main/java/example/project/Main.java
@@ -2,6 +2,6 @@ package example.project;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Software  Softwares  Revsdvengineering!");
+        System.out.println("Software  Soewqr qqw de  Revsdvengineering!");
     }
 }
