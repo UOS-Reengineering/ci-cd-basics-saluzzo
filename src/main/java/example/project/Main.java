e2a32f9787e4790534d8c7dd4ed97bd2ef2b7afa
jinyun <125465742+saluzzo@users.noreply.github.com>
2023-05-03 12:16:21 +0100

    Update Main.java

--- src/main/java/example/project/Main.java
+++ src/main/java/example/project/Main.java
@@ -2,6 +2,6 @@ package example.project;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Software  Soewqr qqw de  Revsdvengineering!");
+        System.out.println("Hello Reengineering");
     }
 }
